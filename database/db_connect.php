604bf9b311e68bf88cdc15c594862eca128b8591
iHakoto <jcgmdz@gmail.com>
2024-11-14 19:43:29 +0800

    Update db_connect.php

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,6 +1,4 @@
 <?php 
 
-$connection= new mysqli('localhost','root','','atis_system_db')or die("Could not connect to mysql".mysqli_error($connection));
+$connection= new mysqli('localhost','u312816882_atisystem','Password123!..','u312816882_atisystem')or die("Could not connect to mysql".mysqli_error($connection));
 ?>
-
-
