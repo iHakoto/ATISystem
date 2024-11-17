97e83657412e8aa325ee364095e665688a78c88a
iHakoto <jcgmdz@gmail.com>
2024-11-17 12:08:23 +0800

    Update registration.php

--- registration.php
+++ registration.php
@@ -19,12 +19,12 @@
             $mail->isSMTP();
             $mail->Host       = 'smtp.gmail.com';
             $mail->SMTPAuth   = true;            
-            $mail->Username   = 'jcgmdz@gmail.com';
+            $mail->Username   = 'willgimena@gmail.com';
             $mail->Password   = 'abqlscqwnkdzirld';                    
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;   
             $mail->Port       = 465;                           
 
-            $mail->setFrom('jcgmdz@gmail.com');
+            $mail->setFrom('willgimena@gmail.com');
             $mail->addAddress($email);
 
             $mail->isHTML(true);
