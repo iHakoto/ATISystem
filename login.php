9c86348ec9385274414dbf1a161d7ae27aec2b90
iHakoto <jcgmdz@gmail.com>
2024-11-21 07:09:41 +0800

    Update login.php

--- login.php
+++ login.php
@@ -47,7 +47,7 @@ ob_end_flush();
     /* Image container: Set it to be square and fill the height */
     .login-image {
         flex: 1;
-        height: 350px; /* Make the image take the full height of the container */
+        height: 62%; /* Make the image take the full height of the container */
         max-width: 450px; /* Set a fixed width to make it a square */
         background: url('../images/alta.jpg') no-repeat center center;
         background-size: cover;
